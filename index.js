617ba186567d9f17f7876f5bb14545862c5c3a44
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-04-19 19:36:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file.
+const lowerCaseDrivers = (drivers) => {
+    return drivers.map(driver => driver.toLowerCase())
+};
+
+const nameToAttributes = (drivers) => {
+    return drivers.map(driver => {
+        let first = driver.split(' ')[0]
+        let last = driver.split(' ')[1]
+        return {firstName: first, lastName: last}
+    })
+};
+
+const attributesToPhrase = (drivers) => {
+    return drivers.map(driver => {
+        return `${driver.name} is from ${driver.hometown}`
+    })
+};
