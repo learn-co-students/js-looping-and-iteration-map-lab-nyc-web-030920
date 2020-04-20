e5209f89c53e68f0092823d78bfa92829d2f3b88
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-19 21:48:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(person) { return person.toLowerCase(); });
+}
+
+// Write lowerCaseDrivers - this function takes an array of drivers, and returns an array of the drivers names in lowercase. Write nameToAttributes - this function takes an array of drivers with their first and last name separated by a space, and returns an array of JavaScript objects with firstName and lastName attributes.
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(person) { 
+    let personArray = person.split(" ");
+    let personObject = {};
+    personObject['firstName'] = personArray[0];
+    personObject['lastName'] = personArray[1];
+    return personObject;
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(person) {
+    return `${person['name']} is from ${person['hometown']}`;
+  });
+}
+
+
+
+
+// Write attributesToPhrase - this function takes an array of drivers as JavaScript objects and returns a string saying "<NAME OF DRIVER> is from <HOMETOWN>" for each JavaScript object in the array. Note that between the < and > we are looking for the value stored in the name and hometown object attributes, not the strings "DRIVER" or "HOMETOWN".
+
+// Conclusion
